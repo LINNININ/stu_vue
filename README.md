42d3f2a34cead6aab4e33de340edcf5f1d91eefd
linnin <PLANCK_20040604@163.com>
2024-09-12 17:29:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+基于人人快速平台\
+颜色修改路径 src/assets/scss/_variables.scss \
+颜色主题库路径 src/element-ui-theme/index.js \
+asd
+
---
+++ src/views/common/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mod-home">
+    <h3>项目介绍</h3>
+    <ul>
+      基于人人快速开发平台
+    </ul>
+
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  .mod-home {
+    line-height: 1.5;
+  }
+</style>
+
